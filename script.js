b1512f958932b50fcc6d13825df936ac42b7b538
Willdeveloppeur <wilfried.rolland.dev@gmail.com>
2019-08-26 10:36:11 +0200

    new version with hour and setTimeout

--- script.js
+++ script.js
@@ -3,29 +3,36 @@ var rep;
 var answer = "Hello !";
 var notText = "Je ne comprends pas votre question";
 var fine = "plutot bien et vous ?";
-// d = new Date(); 
-// var hour =d.toLocaleTimeString();
-// MonTimeout = setTimeout("getAnswer(form)", 3000 ); 
-// // setTimeout(function() { alert("bip"); }, 2000); 
+var question = form.elements['question'].value;
 
 function getAnswer(form) {
-    var question = form.elements['question'].value;
-    var node = document.createElement("DIV");
-    var textnode = document.createTextNode(question);
+    userName();
+    question = form.elements['question'].value.toLowerCase();
+    node = document.createElement("DIV");
+    textnode = document.createTextNode(question);
     node.appendChild(textnode);
     document.getElementById("msg").appendChild(node);
+    timeSet2();
     node.className = "quest";
     document.getElementById("question").value = "";
+    document.getElementById("msg").scrollTop = document.getElementById('msg').scrollHeight;
+    giveTime();
+    return false;
+}
 
-    if (question === "bonjour" || question === "hello" || question === "hey") {
+
+function botAnswer() {
+    if (question === "bonjour" || question === "hello" || question === "hey" || question === "hi" || question === "coucou"  ) {
+        botName();
         node = document.createElement("DIV");
         var textnode = document.createTextNode(answer);
         node.appendChild(textnode);
         document.getElementById("msg").appendChild(node);
         timeSet();
         node.className = "rep";
-        document.getElementById('msg').scrollTop = document.getElementById('msg').scrollHeight;
-    } else if (question === "ca va ?" || question === "la forme ?") {
+        document.getElementById("msg").scrollTop = document.getElementById('msg').scrollHeight;
+    } else if (question === "ca va ?" || question === "la forme ?" || question === "ca farte?" || question === "roots?"  ) {
+        botName();
         node = document.createElement("DIV");
         var textnode = document.createTextNode(fine);
         node.appendChild(textnode);
@@ -33,22 +40,24 @@ function getAnswer(form) {
         timeSet();
         document.getElementById("msg").appendChild(node2);
         node.className = "rep";
-        document.getElementById('msg').scrollTop = document.getElementById('msg').scrollHeight;
+        document.getElementById("msg").scrollTop = document.getElementById('msg').scrollHeight;
     } else {
+        botName();
         node = document.createElement("DIV");
         var textnode = document.createTextNode(notText);
         node.appendChild(textnode);
-        timer = document.getElementById("msg").appendChild(node);
+        document.getElementById("msg").appendChild(node);
         timeSet();
         node.className = "rep2";
-        document.getElementById('msg').scrollTop = document.getElementById('msg').scrollHeight;
+        document.getElementById("msg").scrollTop = document.getElementById('msg').scrollHeight;
     }
-    return false;
-
 }
 
+function giveTime() {
+    setTimeout(botAnswer, 4000);
+}
 
-function timeSet() {
+function timeSet() { //heure pr le bot
     d = new Date();
     var hour = d.toLocaleTimeString();
     node2 = document.createElement("P");
@@ -59,35 +68,76 @@ function timeSet() {
     clearTimeout(timeSet);
 }
 
-function Timer() {
-
-    setTimeout(timer(), 1000);
+function timeSet2() { //heure pr l'utilisateur
+    timeSet();
+    node2.className = "hour2";
 }
 
-// MonTimeout = setTimeout("getAnswer(form)", 3000 ); 
-// // setTimeout(function() { alert("bip"); }, 2000); 
+function botName() {
+    var nameBot = "Bot Simplon";
+    node3 = document.createElement("P");
+    var textnode = document.createTextNode(nameBot);
+    node3.appendChild(textnode);
+    document.getElementById("msg").appendChild(node3);
+    node3.className = "botName";
+}
+
+function userName() {
+    var nameBot = "User";
+    node3 = document.createElement("P");
+    var textnode = document.createTextNode(nameBot);
+    node3.appendChild(textnode);
+    document.getElementById("msg").appendChild(node3);
+    node3.className = "userName";
+}
+
+function afficher_cacher(DIV)
+{
+    if(document.getElementById(msg).style.visibility=="hidden")
+    {
+        document.getElementById(msg).style.visibility="visible";
+        document.getElementById('button_'+msg).innerHTML='Cacher le texte';
+    }
+    else
+    {
+        document.getElementById(msg).style.visibility="hidden";
+        document.getElementById('button_'+msg).innerHTML='Afficher le texte';
+    }
+    return true;
+}
 
 
+    // function botAnswer(){
+    //    
+    // }
 
 
+//     if (question === "bonjour" || question === "hello" || question === "hey") {
+//         node = document.createElement("DIV");
+//         var textnode = document.createTextNode(answer);
+//         node.appendChild(textnode);
+//         document.getElementById("msg").appendChild(node);
+//         timeSet();
+//         node.className = "rep";
+//         document.getElementById("msg").scrollTop = document.getElementById('msg').scrollHeight;
+//     } else if (question === "ca va ?" || question === "la forme ?") {
+//         node = document.createElement("DIV");
+//         var textnode = document.createTextNode(fine);
+//         node.appendChild(textnode);
+//         document.getElementById("msg").appendChild(node);
+//         timeSet();
+//         document.getElementById("msg").appendChild(node2);
+//         node.className = "rep";
+//         document.getElementById("msg").scrollTop = document.getElementById('msg').scrollHeight;
+//     } else {
+//         node = document.createElement("DIV");
+//         var textnode = document.createTextNode(notText);
+//         node.appendChild(textnode);
+//         document.getElementById("msg").appendChild(node);
+//         timeSet();
+//         node.className = "rep2";
+//         document.getElementById("msg").scrollTop = document.getElementById('msg').scrollHeight;
+//     }
+//     return false;
 
-{/* <button id="doorbell-button" class="bottom_right" style="visibility: hidden;">Donner votre avis</button> */ }
-
-
-
-
-
-
-
-
-
-// function ourTime{
-//     question;
-//    
-//     node2 = document.createElement("P");
-//     var hourPlus =document.createTextNode(hour);
-//     node2.appendChild(hourPlus);
-//     document.getElementById("msg").appendChild(node2);
-//     node2.className="hour";
 // }
-// setTimeout(getAnswer(form), 3000);
